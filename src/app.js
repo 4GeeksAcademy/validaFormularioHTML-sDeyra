75c2465c3632252a3fd4a3a88b26312f6f2428a4
sDeyra <sdeymt@gmail.com>
2025-07-30 17:58:51 +0000

    Base formulario

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const cardNumber1 = document.getElementById(validationCustom01)
+const cardNumber2 = document.getElementById(validationCustom02)
+
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
